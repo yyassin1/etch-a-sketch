45d8cf32df9c6aa77a2e0ec0fdca208ea1f99e93
yassin <yassinhussein241@gmail.com>
2023-06-04 12:45:57 -0400

    Add Footer

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
-const box = document.getElementById('box');
-let input = document.getElementById("box_size");
-input.addEventListener("input", function(event) {
-    let rows = event.target.value;
-    let cols = rows;
-    console.log(rows);
-    makeRows(rows, cols)
-});
-//let rows = prompt("Please enter the size of the table");
-function makeRows(rows, cols){
-    box.style.setProperty('--grid-rows', rows);
-    box.style.setProperty('--grid-cols', cols);
+const box = document.getElementById("box");
+let rows = 20;
+let cols = 20;
+makeRows(rows, cols);
+function makeRows(rows, cols) {
+  box.style.setProperty("--grid-rows", rows);
+  box.style.setProperty("--grid-cols", cols);
 
-    for (let r = 0; r < rows; r++){  
-        for (let c = 0; c < cols; c++){
-            let cell = document.createElement("div");
-            cell.className = "grid-item";
-            cell.addEventListener('mouseover', function(){
-                cell.style.backgroundColor = "black";
-            });
-            let clear = document.getElementById("clear");
-            clear.addEventListener("click", function(){
-                cell.style.backgroundColor = "white";
-            })
-            box.appendChild(cell);
-        };
-    };
-};
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      let cell = document.createElement("div");
+      cell.className = "grid-item";
+      cell.addEventListener("mouseover", function () {
+        cell.style.backgroundColor = "black";
+      });
+      let clear = document.getElementById("clear");
+      clear.addEventListener("click", function () {
+        cell.style.backgroundColor = "white";
+      });
+      box.appendChild(cell);
+    }
+  }
+}
